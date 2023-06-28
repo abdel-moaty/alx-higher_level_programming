e18e66b8b31874c55e973fca06dc1f568106e54b
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-06-27 17:40:28 -0700

    Add empty class Square to define square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        No attributes are currently defined.
+
+    Methods:
+        No methods are currently defined.
+    """
+    pass
