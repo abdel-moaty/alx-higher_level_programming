c07ff9888f83430e85ace4bcd6eddf091f18655e
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-08-16 18:41:27 -0700

    Add SQL script to list all shows from hbtn_0d_tvshows_rate by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, IFNULL(SUM(tv_show_ratings.rate), 0) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
