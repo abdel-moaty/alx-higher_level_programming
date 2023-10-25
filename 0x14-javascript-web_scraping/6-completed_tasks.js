e9bfadd1191bd7a82f13661371c7cf3b249e0498
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-10-24 18:31:27 -0700

    Add JavaScript script to compute number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        completed[todo.userId] = (completed[todo.userId] || 0) + 1;
+      }
+    });
+    console.log(completed);
+  }
+});
